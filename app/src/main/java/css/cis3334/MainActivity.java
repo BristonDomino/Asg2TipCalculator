e6ee2ee19e66a92e8a0292f5391cc71a8efbae18
bdddo <Bdomino1@css.edu>
2023-09-19 17:13:38 -0500

    test

---
+++ app/src/main/java/css/cis3334/MainActivity.java
@@ -0,0 +1,60 @@
+package css.cis3334;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText totalBill;
+    EditText numPeople;
+    CheckBox outstandingService;
+    TextView totalTipText;
+    TextView tipPerPersonText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        totalBill = findViewById(R.id.editTextNumberDecimalBillAmount);
+        numPeople = findViewById(R.id.enterNumberPlainTextDecimal);
+        outstandingService = findViewById(R.id.checkBoxOutstandingService);
+        totalTipText = findViewById(R.id.totalTipAmount);
+        tipPerPersonText = findViewById(R.id.tipPerPersonAmount);
+
+        Button calculateBtn = findViewById(R.id.calculatorTIpButton);
+        calculateBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                double bill = Double.parseDouble(totalBill.getText().toString());
+                int people = Integer.parseInt(numPeople.getText().toString());
+
+                // This sets the tip rate to 0.20 if the outstanding service checkbox is check, otherwise sets it to 0.15
+                double tipRate = outstandingService.isChecked() ? 0.20 : 0.15;
+
+                // These calculate the total tip amount and the tip per person
+                double totalTip = bill * tipRate;
+                double tipPerPerson = totalTip / people;
+
+                // Formats the total tip as currency and display it in the "totalTipText" TextView
+                totalTipText.setText(NumberFormat.getCurrencyInstance().format(totalTip));
+
+                //Formats the Tip per person as currency and display it in the "tipPerPersonText" TextView
+                tipPerPersonText.setText(NumberFormat.getCurrencyInstance().format(tipPerPerson));
+            }
+        });
+    }
+
+
+}
